7c2785bd97013bcb1911f23e91cc646ba087dc16
Junaid PV <junu.pv@gmail.com>
2024-09-05 08:54:20 +0530

    Dont set pathauot skipping for paragraphs.

--- src/Commands/FieldUpdate.php
+++ src/Commands/FieldUpdate.php
@@ -514,7 +514,7 @@ class FieldUpdate extends DrushCommands {
               // @see Drupal\content_moderation\Entity\Handler\ModerationHandler::onPresave()
               $entity->setSyncing(TRUE);
             }
-            if ($pathauto_exists) {
+            if ($pathauto_exists && !in_array($entity_type, ['paragraph'])) {
               $entity->path->pathauto = \Drupal\pathauto\PathautoState::SKIP;
             }
             $entity->save();
